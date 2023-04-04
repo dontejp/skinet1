d2dada7f8579e4ec94e85d47a538a9fbad90ebfd
Donte Jean-Pierre <dontejp@gmail.com>
2023-04-04 19:53:46 -0400

    Created a specification evaluator

---
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+        {
+            var query = inputQuery;
+
+            if(spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+            //we are using multiple includes so we have to aggregate them here and it passes it to our query
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+            return query;
+        }
+    }
+}
